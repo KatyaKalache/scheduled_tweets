deee9f6a21bcfdf8c69613a730cb394732e541a1
Ekaterina Kalache <ekaterinakalache@Ekaterinas-MacBook-Air.local>
2023-01-03 11:52:46 +0100

    Added logout, login

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,15 @@ Rails.application.routes.draw do
   # root "articles#index"
   get "about", to: "about#index"
 
+  get "sign_up", to: "registrations#new"
+
+  post "sign_up", to: "registrations#create"
+
+  delete "log_out", to: "sessions#destroy"
+
+  get "sign_in", to: "sessions#new"
+
+  post "sign_in", to: "sessions#create"
+
   root to: "main#index"
 end
